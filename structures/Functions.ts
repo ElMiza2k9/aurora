f1acfe9ac7c6b9bbeef51fb963e0a1ff56ae9f5d
chamln <107578813+chamln@users.noreply.github.com>
2022-07-18 20:31:33 +0300

    chore: update function params

--- structures/Functions.ts
+++ structures/Functions.ts
@@ -16,8 +16,7 @@ export class Functions {
 
   /**
    * Returns a pre-formatted embed
-   * @param {Interaction} interaction
-   * @returns {MessageEmbed}
+   * @param {Interaction} interaction Your interaction (aka slash command)
    */
   buildEmbed(interaction: Interaction) {
     if (!interaction) {
@@ -35,9 +34,8 @@ export class Functions {
 
   /**
    * Returns a formatted time
-   * @param {number} time
+   * @param {number} time Your timestamp in ms
    * @param {TimestampStylesString} type
-   * @returns {number}
    */
   formatTime(time: number, type: TimestampStylesString) {
     return Formatters.time(parseInt(Math.floor(time / 1000).toString()), type);
@@ -45,8 +43,7 @@ export class Functions {
 
   /**
    * Formats a given string so that the first letter is capital
-   * @param {string} str
-   * @returns {string}
+   * @param {string} str String to format
    */
   toCapitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -54,8 +51,7 @@ export class Functions {
 
   /**
    * Formats a given string to escape any markdown symbols
-   * @param {any} str
-   * @returns {string}
+   * @param {any} str String to format
    */
   escapeMd(str: any) {
     return escapeMarkdown(str, {
