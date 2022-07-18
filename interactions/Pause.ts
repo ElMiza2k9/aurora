bff75e908aa268452391e48e0e3baed7bf9fc49a
chamln <107578813+chamln@users.noreply.github.com>
2022-07-18 22:25:44 +0300

    feat: pause command

---
+++ interactions/Pause.ts
@@ -0,0 +1,30 @@
+import { AuroraClient } from "../structures/AuroraClient";
+import { Command } from "../structures/Command";
+
+export default class PauseCommand extends Command {
+  constructor(client: AuroraClient) {
+    super(client, {
+      name: "pause",
+      description: "Pauses music playback",
+    });
+  }
+  async execute(interaction) {
+    const isChecked = await interaction.client.functions.checkVoice(
+      interaction,
+      true
+    );
+    const queue = await interaction.client.player.queues.get(
+      interaction.guild.id
+    );
+
+    if (isChecked === true) {
+      queue.pause();
+      interaction.reply({
+        content: interaction.client.functions.formatReply(
+          "Paused the playback.",
+          interaction.client.config.emojis.pause
+        ),
+      });
+    }
+  }
+}
