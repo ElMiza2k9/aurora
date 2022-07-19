4e1988ee6449f41c987f4c3f73c182b34aec75d9
chamln <107578813+chamln@users.noreply.github.com>
2022-07-19 15:12:50 +0300

    chore: format code

--- events/PlayerAddList.ts
+++ events/PlayerAddList.ts
@@ -10,7 +10,9 @@ export default class PlayerAddListEvent extends Event {
   async execute(client: AuroraClient, _queue: Queue, playlist: Playlist<any>) {
     playlist.metadata.i.followUp({
       content: client.functions.formatReply(
-        `Added **${client.functions.escapeMd(playlist.name)}** (${playlist.songs.length} songs) to the queue.`,
+        `Added **${client.functions.escapeMd(playlist.name)}** (${
+          playlist.songs.length
+        } songs) to the queue.`,
         client.config.emojis.check_mark
       ),
     });
