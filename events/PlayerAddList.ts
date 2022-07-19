1ae01382925c8b0c0b747e66cf3b85c76c935c87
chamln <107578813+chamln@users.noreply.github.com>
2022-07-19 13:00:37 +0300

    feat: addList event

---
+++ events/PlayerAddList.ts
@@ -0,0 +1,18 @@
+import { Playlist, Queue } from "distube";
+import { AuroraClient } from "../structures/AuroraClient";
+import { Event } from "../structures/Event";
+
+export default class PlayerAddListEvent extends Event {
+  constructor(client: AuroraClient) {
+    super(client, "addList", false, true);
+  }
+
+  async execute(client: AuroraClient, _queue: Queue, playlist: Playlist<any>) {
+    playlist.metadata.i.followUp({
+      content: client.functions.formatReply(
+        `Added **${client.functions.escapeMd(playlist.name)}** (${playlist.songs.length} songs) to the queue.`,
+        client.config.emojis.check_mark
+      ),
+    });
+  }
+}
