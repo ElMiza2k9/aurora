efdebac4851fd9ac6bcd1aa0e77145bac4d864c2
chamln <107578813+chamln@users.noreply.github.com>
2022-07-27 14:59:28 +0300

    feat: server command

---
+++ interactions/Server.ts
@@ -0,0 +1,85 @@
+import { ChannelType } from "discord.js";
+import { AuroraClient } from "../structures/AuroraClient";
+import { Command } from "../structures/Command";
+
+export default class ServerCommand extends Command {
+  constructor(client: AuroraClient) {
+    super(client, {
+      name: "server",
+      description: "Get the info about this server",
+    });
+  }
+  async execute(interaction) {
+    const guild = interaction.guild;
+    const serverOwner = await guild.fetchOwner();
+
+    interaction.reply({
+      embeds: [
+        interaction.client.functions
+          .buildEmbed(interaction)
+          .setTitle(
+            `${interaction.client.functions.escapeMd(guild.name)} (ID ${
+              guild.id
+            })`
+          )
+          .setThumbnail(guild.iconURL())
+          .setDescription(
+            guild.description
+              ? interaction.client.functions.escapeMd(guild.description)
+              : `This server has no description.`
+          )
+          .addFields([
+            {
+              name: "General",
+              value: `**Server ID:** ${guild.id}
+        **Created by:** ${serverOwner.user} (ID ${serverOwner.user.id})
+        **Creation date:** ${interaction.client.functions.formatTime(
+          guild.createdTimestamp,
+          "R"
+        )}
+        `,
+            },
+            {
+              name: "Members",
+              value: `
+          **Total:** ${guild.members.cache.size}
+          **Humans:** ${guild.members.cache.filter((m) => !m.user.bot).size}
+          **Bots:** ${guild.members.cache.filter((m) => m.user.bot).size}
+          `,
+              inline: true,
+            },
+            {
+              name: "Channels",
+              value: `
+    **Total:** ${guild.channels.cache.size}
+    **Text:** ${
+      guild.channels.cache.filter((c) => c.type === ChannelType.GuildText).size
+    }
+    **Voice:** ${
+      guild.channels.cache.filter((c) => c.type === ChannelType.GuildVoice).size
+    }
+    **Stages:** ${
+      guild.channels.cache.filter((c) => c.type === ChannelType.GuildStageVoice)
+        .size
+    }
+    `,
+              inline: true,
+            },
+            {
+              name: "Boosts",
+              value: `
+        **Boosts count:** ${guild.premiumSubscriptionCount}
+        **Level:** ${guild.premiumTier}`,
+              inline: true,
+            },
+          ])
+          .setImage(
+            guild.bannerURL({
+              size: 2048,
+              format: "png",
+            })
+          ),
+      ],
+    });
+  }
+}
