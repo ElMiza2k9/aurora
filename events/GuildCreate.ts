8bba69933ae386caeeaaae1fb33459237ebceedc
chamln <107578813+chamln@users.noreply.github.com>
2022-07-31 15:18:19 +0300

    chore: add guild to database on create

---
+++ events/GuildCreate.ts
@@ -0,0 +1,14 @@
+import * as DJS from "discord.js";
+import { AuroraClient } from "../structures/AuroraClient";
+import { Event } from "../structures/Event";
+
+export default class GuildCreateEvent extends Event {
+  constructor(client: AuroraClient) {
+    super(client, "guildCreate");
+  }
+
+  async execute(client: AuroraClient, guild: DJS.Guild) {
+    if (!guild) return;
+    await client.functions.addGuild(guild.id);
+  }
+}
