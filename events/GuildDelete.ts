3ef892053ea0bab1af0a2c5a530d84782a978629
chamln <107578813+chamln@users.noreply.github.com>
2022-07-31 15:18:30 +0300

    chore: remove guild from database on remove

---
+++ events/GuildDelete.ts
@@ -0,0 +1,14 @@
+import * as DJS from "discord.js";
+import { AuroraClient } from "../structures/AuroraClient";
+import { Event } from "../structures/Event";
+
+export default class GuildDeleteEvent extends Event {
+  constructor(client: AuroraClient) {
+    super(client, "guildDelete");
+  }
+
+  async execute(client: AuroraClient, guild: DJS.Guild) {
+    if (!guild) return;
+    await client.functions.deleteGuild(guild.id);
+  }
+}
