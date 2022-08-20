a7ea2ecc28018659c4a833b85e588e270f66bfed
chamln <107578813+chamln@users.noreply.github.com>
2022-08-20 17:53:31 +0300

    chore: rename BaseCommand to Command

--- structures/Command.ts
+++ structures/Command.ts
@@ -1,3 +1,39 @@
-import { BaseCommand } from "./BaseCommand";
+import * as DJS from "discord.js";
+import { AuroraClient } from "./AuroraClient";
 
-export abstract class Command extends BaseCommand {}
+export interface CommandOptions {
+  name: string;
+  description: string;
+  client_perms?: Array<DJS.PermissionResolvable>;
+  user_perms?: Array<DJS.PermissionResolvable>;
+  options?: DJS.ApplicationCommandOptionData[];
+}
+
+export abstract class Command<
+  TOptions extends CommandOptions = CommandOptions
+> {
+  protected _options: TOptions;
+  name: string;
+  client_perms?: Array<DJS.PermissionResolvable>;
+  user_perms?: Array<DJS.PermissionResolvable>;
+  client: AuroraClient;
+
+  constructor(client: AuroraClient, options: TOptions) {
+    this.client = client;
+    this.name = options.name;
+    this.client_perms = options.client_perms;
+    this.user_perms = options.user_perms;
+    this._options = options;
+
+    this.execute = this.execute.bind(this);
+  }
+
+  get options(): TOptions {
+    return this._options;
+  }
+
+  abstract execute(
+    interaction: DJS.CommandInteraction,
+    locale: any
+  ): Promise<unknown>;
+}
--- structures/SubCommand.ts
+++ structures/SubCommand.ts
@@ -1,12 +1,12 @@
-import { BaseCommand, BaseCommandOptions } from "./BaseCommand";
+import { Command, CommandOptions } from "./Command";
 import * as DJS from "discord.js";
 
-export interface SubCommandOptions extends BaseCommandOptions {
+export interface SubCommandOptions extends CommandOptions {
   topName: string;
   groupName?: string;
 }
 
-export abstract class SubCommand extends BaseCommand<SubCommandOptions> {
+export abstract class SubCommand extends Command<SubCommandOptions> {
   get options(): SubCommandOptions & {
     type: DJS.ApplicationCommandOptionType.Subcommand;
   } {
