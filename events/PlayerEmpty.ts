26b1c8fe17474e4eafc44eba235eaa5c16a04b10
chamln <107578813+chamln@users.noreply.github.com>
2022-08-23 12:43:31 +0300

    fix: incorrect class name

--- events/PlayerEmpty.ts
+++ events/PlayerEmpty.ts
@@ -2,16 +2,13 @@ import { Queue } from "distube";
 import { AuroraClient } from "../structures/AuroraClient";
 import { Event } from "../structures/Event";
 
-export default class PlayerAddListEvent extends Event {
+export default class PlayerEmptyEvent extends Event {
   constructor(client: AuroraClient) {
     super(client, "empty", false, true);
   }
 
   async execute(client: AuroraClient, queue: Queue) {
-    const l = await client.locales.getLocale(
-      queue.id,
-      queue!.songs[0].user!.id
-    );
+    const l = await client.locales.getLocale(queue.id, queue.songs[0].user!.id);
     queue.textChannel?.send({
       content: client.reply(l("misc:music:stopped"), ":pensive:"),
     });
