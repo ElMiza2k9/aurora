e8acdfde2b1b47776d1a2d3ed5d75bbcbf32ad8f
chamln <107578813+chamln@users.noreply.github.com>
2022-08-27 12:30:41 +0300

    feat: more roleplay commands

---
+++ interactions/roleplay/Pat.ts
@@ -0,0 +1,48 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { AuroraClient } from "../../structures/AuroraClient";
+import { SubCommand } from "../../structures/SubCommand";
+
+export default class ChannelCreateCommand extends SubCommand {
+  constructor(client: AuroraClient) {
+    super(client, {
+      name: "pat",
+      topName: "roleplay",
+      description: "Pat someone!",
+      options: [
+        {
+          name: "user",
+          type: ApplicationCommandOptionType.User,
+          description: "A person you want to pat",
+          required: true,
+        },
+      ],
+    });
+  }
+  async execute(interaction, l) {
+    await interaction.deferReply();
+    const user = interaction.options.getUser("user");
+
+    if (user.id === this.client.user.id || user.id === interaction.user.id) {
+      return interaction.followUp({
+        content: this.client.reply(
+          l("commands:roleplay:pat:checks:self"),
+          ":face_with_raised_eyebrow:"
+        ),
+      });
+    } else {
+      const gif = await fetch(`https://nekos.life/api/v2/img/pat`).then((res) =>
+        res.json()
+      );
+      await interaction.followUp({
+        content: this.client.reply(
+          l("commands:roleplay:pat:reply", {
+            user: `${user}`,
+            author: `${interaction.user}`,
+          }),
+          ":hugging:"
+        ),
+        embeds: [this.client.embed(interaction).setImage(gif.url)],
+      });
+    }
+  }
+}
---
+++ interactions/roleplay/Slap.ts
@@ -0,0 +1,48 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { AuroraClient } from "../../structures/AuroraClient";
+import { SubCommand } from "../../structures/SubCommand";
+
+export default class ChannelCreateCommand extends SubCommand {
+  constructor(client: AuroraClient) {
+    super(client, {
+      name: "slap",
+      topName: "roleplay",
+      description: "Slap someone!",
+      options: [
+        {
+          name: "user",
+          type: ApplicationCommandOptionType.User,
+          description: "A person you want to slap",
+          required: true,
+        },
+      ],
+    });
+  }
+  async execute(interaction, l) {
+    await interaction.deferReply();
+    const user = interaction.options.getUser("user");
+
+    if (user.id === this.client.user.id || user.id === interaction.user.id) {
+      return interaction.followUp({
+        content: this.client.reply(
+          l("commands:roleplay:slap:checks:self"),
+          ":face_with_raised_eyebrow:"
+        ),
+      });
+    } else {
+      const gif = await fetch(`https://nekos.life/api/v2/img/slap`).then(
+        (res) => res.json()
+      );
+      await interaction.followUp({
+        content: this.client.reply(
+          l("commands:roleplay:slap:reply", {
+            user: `${user}`,
+            author: `${interaction.user}`,
+          }),
+          ":hugging:"
+        ),
+        embeds: [this.client.embed(interaction).setImage(gif.url)],
+      });
+    }
+  }
+}
