17c75c74c200e8643a10ba8c97787389b5bee68a
chamln <107578813+chamln@users.noreply.github.com>
2022-09-05 18:37:04 +0300

    chore: add easter egg in info-user command
    Signed-off-by: chamln <107578813+chamln@users.noreply.github.com>

--- interactions/info/User.ts
+++ interactions/info/User.ts
@@ -27,10 +27,25 @@ export default class InfoUserCommand extends SubCommand {
     const user = interaction.options.getUser("user");
     const guildMember = interaction.guild.members.cache.get(user.id);
     const userFlags = await user.fetchFlags();
+    const date = new Date(Date.now());
 
     if (userFlags.has(UserFlags.TeamPseudoUser)) {
       return interaction.followUp({
         content: this.client.reply(l("commands:info:user:pseudo_user"), ":x:"),
+      });
+    }
+
+    // You've found an easter egg!
+    if (
+      date.getUTCDate() === 6 &&
+      date.getUTCMonth() + 1 === 11 &&
+      user.id === "718457540878532650"
+    ) {
+      return interaction.followUp({
+        content: this.client.reply(
+          l("misc:easter_eggs:emil_cake_day"),
+          ":cake:"
+        ),
       });
     }
 
@@ -42,8 +57,9 @@ export default class InfoUserCommand extends SubCommand {
         ":bust_in_silhouette:"
       ),
       embeds: [
-        (await this.client
-          .embed(interaction))
+        (
+          await this.client.embed(interaction)
+        )
           .setThumbnail(user.avatarURL())
           .addFields([
             {
