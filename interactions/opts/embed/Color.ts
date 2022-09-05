80c7313303bd46a87f07882b498f1dcae4d27c7e
chamln <107578813+chamln@users.noreply.github.com>
2022-09-05 20:36:09 +0300

    feat: add embed color customization
    Signed-off-by: chamln <107578813+chamln@users.noreply.github.com>

---
+++ interactions/opts/embed/Color.ts
@@ -0,0 +1,49 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { AuroraClient } from "../../../structures/AuroraClient";
+import { SubCommand } from "../../../structures/SubCommand";
+
+export default class OptsEmbedColorCommand extends SubCommand {
+  constructor(client: AuroraClient) {
+    super(client, {
+      name: "color",
+      groupName: "embed",
+      topName: "opts",
+      description: "Edit the embeds color for this server",
+      options: [
+        {
+          name: "color",
+          type: ApplicationCommandOptionType.String,
+          description: "The color you want to set (HEX, # allowed)",
+          required: true,
+        },
+      ],
+    });
+  }
+  async execute(interaction, l) {
+    await interaction.deferReply();
+    const color = interaction.options.getString("color");
+
+    const hexRegEx = /^#?([0-9a-f]{6})$/i;
+
+    if (!hexRegEx.test(color)) {
+      return interaction.followUp({
+        content: this.client.reply(
+          l("commands:opts:embed:color:invalid_color"),
+          ":x:"
+        ),
+      });
+    }
+    await this.client.updateGuild(interaction.guild.id, {
+      embed: { color: color.replace("#", "") },
+    });
+
+    await interaction.followUp({
+      content: this.client.reply(
+        l("commands:opts:embed:color:reply", {
+          color: `${color.replace("#", "")}`,
+        }),
+        ":art:"
+      ),
+    });
+  }
+}
